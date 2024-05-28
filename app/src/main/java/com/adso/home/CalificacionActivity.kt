9342da003b9cb613d7b9e14ba8977d58381f9997
Dani2000512 <dani1990marcela@gmail.com>
2024-05-28 17:20:30 -0500

    Arreglado

---
+++ app/src/main/java/com/adso/home/CalificacionActivity.kt
@@ -0,0 +1,33 @@
+package com.adso.home
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class CalificacionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calificacion)
+
+        val publicacionesTextView = findViewById<TextView>(R.id.publicacionesTextView)
+        val calificacionesTextView = findViewById<TextView>(R.id.calificacionesTextView)
+        val skillsTextView = findViewById<TextView>(R.id.skillsTextView)
+
+
+        publicacionesTextView.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        calificacionesTextView.setOnClickListener {
+            val intent = Intent(this, CalificacionActivity::class.java)
+            startActivity(intent)
+        }
+        skillsTextView.setOnClickListener {
+            val intent = Intent(this, SkillsActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
